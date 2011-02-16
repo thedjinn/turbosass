169a9a460e4c91cea4ebbeedb71fd6cb46832616
Emil Loer <emil@koffietijd.net>
2011-02-16 16:48:12 +0100

    modularize loader

--- lib/turbosass.rb
+++ lib/turbosass.rb
@@ -1,5 +1,12 @@
 require "sass"
 require "sass/plugin"
 
+module TurboSass
+  def self.path
+    File.expand_path(File.join(File.dirname(__FILE__), "turbosass", "scss"))
+  end
+end
+
+# Inject the TurboSass SCSS path
 Sass::Plugin.options[:load_paths] ||= []
-Sass::Plugin.options[:load_paths] << File.expand_path(File.join(File.dirname(__FILE__), "scss"))
+Sass::Plugin.options[:load_paths] << TurboSass.path
