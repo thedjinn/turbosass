dc98c64ac0f5c071909aebd59508a94d17494d3b
Emil Loer <emil@koffietijd.net>
2011-03-12 14:57:13 +0100

    add rails support

--- lib/turbosass.rb
+++ lib/turbosass.rb
@@ -1,5 +1,9 @@
-require "sass"
-require "sass/plugin"
+if defined? Rails
+  require "haml"
+else
+  require "sass"
+  require "sass/plugin"
+end
 
 module TurboSass
   def self.path
@@ -8,11 +12,17 @@ module TurboSass
 end
 
 module Sass
-  class Engine
-    new_default_options = DEFAULT_OPTIONS.clone
-    new_default_options[:load_paths] << ::TurboSass.path
+  if defined? Engine
+    class Engine
+      new_default_options = DEFAULT_OPTIONS.clone
+      new_default_options[:load_paths] << ::TurboSass.path
 
-    remove_const(:DEFAULT_OPTIONS)
-    DEFAULT_OPTIONS = new_default_options.freeze
+      remove_const(:DEFAULT_OPTIONS)
+      DEFAULT_OPTIONS = new_default_options.freeze
+    end
+  elsif defined? Plugin
+    module Plugin
+      @options[:load_paths] = [::TurboSass.path]
+    end
   end
 end
